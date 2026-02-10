06efc3c799bc42d8b97ba93f0e2dbf8b17ea828e
Muhamad Adnan Awaluddin <adnanawaluddin8115@gmail.com>
2026-02-10 20:20:46 +0700

    Delete Mobil directory

--- Mobil/Mazda/CX9/perhitungan.js
+++
@@ -1,9 +0,0 @@
-function hitung() {
-    let harga = 955000000;
-    let ongkir = 1000000;
-    let DPP = 2000;
-	let PPN = 300000;
-	let Jumlah = parseInt(document.getElementById("jumlah").value);
-	let total = harga * Jumlah;
-	let totalAkhir = total + ongkir + DPP + PPN;
-    document.getElementById('total').innerHTML = totalAkhir;}
